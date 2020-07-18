f2bdb223fe626e3ff6c80f26d507baa304d09008
terland <haavard10@gmail.com>
2020-07-18 20:24:40 +0200

    get description of job ad

--- script/main.py
+++ script/main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 bearercode = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ'
 
@@ -7,4 +8,5 @@ endpoint = 'https://arbeidsplassen.nav.no/public-feed/api/v1/ads'
 data = {}
 
 r = requests.get(endpoint,data = data,headers = headers)
-print(r)
+data = r.json()
+print(data['content'][0]['description'])
