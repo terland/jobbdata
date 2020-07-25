5bd2ba4ff945017d2f1090714c0a52ffa7b75d6d
terland <haavard10@gmail.com>
2020-07-25 20:18:36 +0200

    set up for bag-of-words modelling

--- script/description_aggregation.py
+++ script/description_aggregation.py
@@ -5,15 +5,12 @@ import re
 # Clean text
 def clean_html(desc):
     desc = desc.lower()
-    desc = re.sub('<.>|</.>|<..>|</..>',' ',desc)
+    desc = re.sub('<.>|</.>|<..>|</..>|<strong>|</strong>',' ',desc)
     desc = re.sub(' +',' ',desc)
     desc = desc.lower()
     return(desc)
 
-print(clean_html('<a/> Www <p> pwpwpda </p>'))
-
 # Get all job-descriptions, along with titles.
-
 all_jobs = pd.DataFrame()
 
 
@@ -23,11 +20,11 @@ description_list = []
 data = get_jobs()
 for job in data['content']:
     title_list.append(str(job['jobtitle']) + " @ " + job['title'])
-    
-
-
-    description_list.append(str(job['description']))
+    description_list.append(clean_html(str(job['description'])))
 
 all_jobs['title'] = title_list
 all_jobs['description'] = description_list
+
+# Implement bag-of-words modelling of data
+
 print(all_jobs)
